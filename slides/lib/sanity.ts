9d6c9f9facf5fa9884aff238b7640b44d1370408
nshahidi <nshahidi@webstacks.com>
2026-03-07 14:33:27 -0800

    fix: remove turbo flag from dev script and set default values for Sanity client configuration

--- slides/lib/sanity.ts
+++ slides/lib/sanity.ts
@@ -3,8 +3,8 @@ import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "mf2ypiyj",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-15",
   useCdn: true,
 });
